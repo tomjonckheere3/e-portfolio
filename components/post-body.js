d6cf80d29e3b9afdb4f2dec02f65d5199e85686a
tomjonckheere3 <tom.jonckheere3@student.howest.be>
2022-05-23 00:05:34 +0200

    add crossOrigin

--- components/post-body.js
+++ components/post-body.js
@@ -11,7 +11,7 @@ export default function PostBody({ title, coverImage, date, content }) {
                 </h1>
             </div>
             <div className="flex-shrink-0 mt-6 lg:mt-12 mb-4">
-                <CoverImage title={title} imageUrl={coverImage.url} />
+                <CoverImage title={title} imageUrl={coverImage?.url} />
             </div>
             <div className="mb-4">
                 <Date dateString={date} bold={true} />
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,12 +1,20 @@
 import Head from 'next/head'
+import { useRouter } from "next/router";
+import ErrorPage from 'next/error'
 import PostBody from "../../components/post-body";
 import { getAllPosts, getPost } from "../../lib/graphcms";
 
 export default function Post({ post }) {
+    const router = useRouter()
+
+    if (!router.isFallback && !post?.slug) {
+        return <ErrorPage statusCode={404} />
+    }
+
     return (
         <div className="space-y-14 lg:space-y-24">
             <Head>
-                <title>Tom Jonckheere</title>
+                <title>Tom Jonckheere - {post?.title}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerpolicy="no-referrer" /> 
@@ -14,7 +22,7 @@ export default function Post({ post }) {
 
             <main className="max-w-4xl mx-auto mt-8 md:mt-16 antialiased">
                 <PostBody
-                    title={post.title}
+                    title={post?.title}
                     coverImage={post.coverImage}
                     date={post.date}
                     content={post.content}
@@ -40,7 +48,7 @@ export async function getStaticProps({ params }) {
       paths: posts.map(({ slug }) => ({
         params: { slug },
       })),
-      fallback: true,
+      fallback: false,
     }
   }
   
